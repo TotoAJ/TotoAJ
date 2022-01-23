2f3abdb963277106c5a980b35f55612edd407a54
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2022-01-22 20:53:52 -0800

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include "tlib.hpp"
+
+DWORD GetProcId(const char* procName) {
+	DWORD procId = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
+		procEntry.dwSize = sizeof(procEntry);
+
+		if (Process32First(hSnap, &procEntry)) {
+			do {
+				if (!_stricmp(procEntry.szExeFile, procName)) {
+					procId = procEntry.th32ProcessID;
+					break;
+				}
+			} while (Process32Next(hSnap, &procEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return procId;
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam) {
+	DWORD dwThreadId, dwProcessId;
+	HINSTANCE hInstance;
+	char String[255];
+	if (!hWnd)
+		return TRUE;
+	if (!::IsWindowVisible(hWnd))
+		return TRUE;
+	if (!SendMessage(hWnd, WM_GETTEXT, sizeof(String), (LPARAM)String))
+		return TRUE;
+	hInstance = (HINSTANCE)GetWindowLong(hWnd, 0);
+	dwThreadId = GetWindowThreadProcessId(hWnd, &dwProcessId);
+	std::cout << "PID: " << dwProcessId << '\t' << String << '\t' << std::endl;
+	return TRUE;
+}
+
+int main() {
+	const char* dllPath = "C:\\PATH\\Dll2.dll";
+	DWORD procId = 0;
+	std::cout << "Available Targets:\n" << std::endl;
+	EnumWindows(EnumWindowsProc, NULL);
+	std::cin >> procId;
+	
+
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+	if (hProc && hProc != INVALID_HANDLE_VALUE) {
+		void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		if (loc) {
+			WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+		}
+
+		HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+
+		if (hThread) {
+			CloseHandle(hThread);
+		}
+	}
+
+	if (hProc) {
+		CloseHandle(hProc);
+	}
+
+	tlib::ExitProgram(tlib::Exit_Success);
+}
