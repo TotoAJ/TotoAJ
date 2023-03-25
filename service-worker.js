60566b37818f116f6b470bcecb21a68c88ef58c2
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2023-03-25 13:43:16 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+if( 'function' === typeof importScripts) {
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+   
+    workbox.routing.registerRoute(
+        ({request}) => request.destination === 'image',
+        new workbox.strategies.NetworkFirst()
+    );
+}
